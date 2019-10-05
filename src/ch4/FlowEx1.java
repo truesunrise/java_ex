9348125075284237466bd4de27f405926ca72168
didwlstjs <jsunrsise999@gmail.com>
2019-10-05 16:44:59 +0900

    191005/양진선: if 조건문

---
+++ src/ch4/FlowEx1.java
@@ -0,0 +1,30 @@
+package ch4;
+
+public class FlowEx1 {
+    public static void main(String[] args) {
+        int x = 0;
+        System.out.printf("x=%d 일 때, 참인 것은%n", x);
+
+        if(x == 0) System.out.println("x == 0");
+        if(x != 0) System.out.println("x != 0");
+        if(! (x == 0)) System.out.println("! (x != 0)");
+        if(! (x != 0)) System.out.println("! (x != 0)");
+
+        x = 1;
+        System.out.printf("x=%d 일 때, 참인 것은%n", x);
+
+//        x=0 일 때, 참인 것은
+//        x == 0
+//        ! (x != 0)
+
+        if(x == 0) System.out.println("x == 0");
+        if(x != 0) System.out.println("x != 0");
+        if(! (x == 0)) System.out.println("! (x == 0)");
+        if(! (x != 0)) System.out.println("! (x != 0)");
+
+//        x=1 일 때, 참인 것은
+//        x != 0
+//        ! (x == 0)
+
+    }
+}
