3b84d96326aa41e743f4dc605d6595c60cd50938
JinSeon Yang <jsunrise999@gmail.com>
2019-10-13 23:01:08 +0900

    191013/양진선: 중첩 for문 - 별찍기

---
+++ src/ch4/FlowEx16.java
@@ -0,0 +1,12 @@
+package ch4;
+
+public class FlowEx16 {
+    public static void main(String[] args) {
+        for(int i = 1; i <= 5; i++) {
+            for(int j = 0; j <= 10; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
