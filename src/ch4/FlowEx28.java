a3c6a99fdee055b55cc9a0b5460ba7ca6dca1e32
JinSeon Yang <jsunrise999@gmail.com>
2019-10-19 20:28:31 +0900

    191019/양진선: do - while문 - 369게임

--- src/ch4/FlowEx28.java
+++ src/ch4/FlowEx28.java
@@ -6,7 +6,10 @@ import java.util.Scanner;
  * do - while문
  *
  * do { 조건식의 연산결과가 참일 때 수행될 문장들을 적는다. } while (조건식);
+ *
  * 끝에 ';'를 잊지 않도록 주의
+ * while문과 반대로 블럭 {}을 먼저 수행한 후에 조건식을 평가
+ * while문은 조건식의 결과에 따라 블럭 {}이 한 번도 수행되지 않을 수 있지만 do-while문은 최소 한번은 수행될 것을 보장
  */
 public class FlowEx28 {
     public static void main(String[] args) {
